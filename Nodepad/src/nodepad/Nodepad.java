18316673ee46c392edfede64148070a08e8074cf
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-11-02 23:45:53 -0800

    Created Project - Nodepad

---
+++ Nodepad/src/nodepad/Nodepad.java
@@ -0,0 +1,17 @@
+
+package nodepad;
+
+/**
+ *
+ * @author MdMahadiHasan
+ */
+public class Nodepad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
